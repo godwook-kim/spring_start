ea2dad250f573b1f7476b6b790c382a3a8896d9f
godwook <passionate.ksw@gmail.com>
2023-01-31 21:31:44 +0900

    인터페이스에만 의존하도록 수정 (테스트 코드)

--- core/src/test/java/hello/core/order/OrderServiceTest.java
+++ core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -6,15 +6,20 @@ import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    AppConfig appConfig = new AppConfig();
-    MemberService memberService = appConfig.memberService();
-    //MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = appConfig.orderService();
-    //OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
 
+    @BeforeEach
+    public void beForeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+
+    }
 
     @Test
     void createOrder(){
